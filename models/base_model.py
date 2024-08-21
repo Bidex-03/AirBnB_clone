c5757349a4c180574dfcff163338c7541e086682
daadesina <daadesina1@gmail.com>
2024-08-21 08:01:59 +0100

    bidex

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A model
+"""
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    '''
+    a BaseModel class
+    '''
+    def __init__(self, *args, **kwargs):
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+
+        return my_dict
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
+
